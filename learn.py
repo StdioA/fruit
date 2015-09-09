ff85a3f943bd861b83a7269f79ef800629aaa6cf
Stdio A <stdio_dai@nuaa.edu.cn>
2015-09-09 21:58:32 +0800

    Add stripe vector

--- learn.py
+++ learn.py
@@ -13,14 +13,16 @@ def learn(filename):
     names = [x[0] for x in data]
     color = [x[1] for x in data]
     geo = [x[2] for x in data]
-    LBP = [x[3] for x in data]
+    # LBP = [x[3] for x in data]
 
     ndata = []
 
     # print np.array(LBP).shape
 
-    for c, g, l in zip(color, geo, LBP):
-        ndata.append(np.concatenate((c, [g], l.flatten())))
+    # for c, g, l in zip(color, geo, LBP):
+    #     ndata.append(np.concatenate((c, [g], l.flatten())))
+    for c, g in zip(color, geo):
+            ndata.append(np.concatenate((c, [g])))
 
     clf.fit(ndata, names)
 
@@ -29,14 +31,16 @@ def learn(filename):
 def predict():
     # print clf.predict([94, 244, 89])
     # print clf.predict([80, 254, 215])
-    a = main.main(r'F:\SC\fruits\banana.jpg')
-    color, geo, LBP = a[1], a[2], a[3]
+    a = main.main(r'F:\SC\fruits\orange2.jpg')
+    # color, geo, LBP = a[1], a[2], a[3]
+    color, geo = a[1], a[2]
 
-    return clf.predict(np.concatenate((color, [geo], LBP.flatten())))
+    # return clf.predict(np.concatenate((color, [geo], LBP.flatten())))
+    return clf.predict(np.concatenate((color, [geo])))
 
 if __name__ == '__main__':
     clf = svm.SVC(gamma=0.001, C=100.)
     # clf = cluster.KMeans()
-    learn("data_with_LBP.dat")
+    learn("data.dat")
     print predict()[0]
     
